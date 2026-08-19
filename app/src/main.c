9a92e9cf983485e57cc57b359e0f961ab1329136
David Gonzalez <dgonzalez97@gmail.com>
2026-08-20 00:47:17 +0200

    [COMPOSE][LOG] Integrate logging service

--- app/src/main.c
+++ app/src/main.c
@@ -1,9 +1,11 @@
 #include <zephyr/kernel.h>
 
 #include <kfsw/services/boot.h>
+#include <kfsw/services/log.h>
 
 int main(void)
 {
+    kfsw_log_info("K-FSW application starting");
     kfsw_boot_service_start();
 
     for (;;) {
